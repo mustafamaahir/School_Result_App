813518551735b01c4f6da464b4a1e61dfd487860
unknown <mustafamaahir@gmail.com>
2026-05-15 22:53:37 +0100

    new updatee

--- frontend_react/src/components/AdminMenu.jsx
+++ frontend_react/src/components/AdminMenu.jsx
@@ -1,9 +1,9 @@
-// // src/components/AdminMenu.jsx
+// src/components/AdminMenu.jsx
 // import React from "react";
-// import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-// export default function AdminMenu() {
-//   const location = useLocation();
-//   const linkStyle = (p) => ({ textDecoration: "none", color: location.pathname === p ? "#007bff" : "#000" });
+export default function AdminMenu() {
+  const location = useLocation();
+  const linkStyle = (p) => ({ textDecoration: "none", color: location.pathname === p ? "#007bff" : "#000" });
   
-// }
+}
