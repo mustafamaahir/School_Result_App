d34975b8b284afd6c96086d1ced26866d7227dea
unknown <mustafamaahir@gmail.com>
2026-05-15 21:20:16 +0100

    new update

--- frontend_react/src/api/api.js
+++ frontend_react/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000"; 
+const API_URL = "https://school-result-app-3k2s.onrender.com"; 
 
 export const api = axios.create({
   baseURL: API_URL,
