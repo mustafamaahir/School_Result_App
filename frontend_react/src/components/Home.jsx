46f13e3b94ce0f7566f13e86d77a8a730b5c9cae
unknown <mustafamaahir@gmail.com>
2026-05-16 09:29:19 +0100

    Remove a comment

--- frontend_react/src/components/Home.jsx
+++ frontend_react/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import heroImage from "../assets/home-hero.png"; // adjust path relative to Home.jsx
+import heroImage from "../assets/home-hero.png";
 
 export default function Home({ user }) {
   return (
